c330d73f1fd83b469d10bbf00c161df3d8682dd1
FSEVERI\reginato2906 <FSEVERI\reginato2906@lmatbi-14.fseveri.it>
2016-02-23 08:45:14 +0100

    Initialized project

---
+++ src/octocat/Octocat.java
@@ -0,0 +1,16 @@
+package octocat;
+
+/**
+ *
+ * @author FSEVERI\reginato2906
+ */
+public class Octocat {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
